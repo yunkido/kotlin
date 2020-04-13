0d9bbe945ae0204140f1f3c7732f07d774f0238c
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-04-13 18:23:03 +0300

    Introduce FIR serializer (isn't in use yet)

---
+++ compiler/fir/fir-serialization/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+repositories {
+    maven(url = "https://dl.bintray.com/kotlin/kotlinx")
+}
+
+dependencies {
+    compile(project(":core:descriptors"))
+    compile(project(":compiler:fir:cones"))
+    compile(project(":compiler:fir:tree"))
+    compile(project(":compiler:fir:resolve"))
+
+    compileOnly(project(":kotlin-reflect-api"))
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core", rootProject = rootProject) }
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" { none() }
+}

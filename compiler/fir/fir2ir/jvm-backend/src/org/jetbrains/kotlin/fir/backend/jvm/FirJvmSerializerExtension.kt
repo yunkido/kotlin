731e7ffb9ad0ede726ef6e7a50340994f0c21a61
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-04-21 18:42:57 +0300

    FIR serializer: introduce nonSourceAnnotations

--- compiler/fir/fir2ir/jvm-backend/src/org/jetbrains/kotlin/fir/backend/jvm/FirJvmSerializerExtension.kt
+++ compiler/fir/fir2ir/jvm-backend/src/org/jetbrains/kotlin/fir/backend/jvm/FirJvmSerializerExtension.kt
@@ -22,6 +22,7 @@ import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.resolve.inference.isBuiltinFunctionalType
 import org.jetbrains.kotlin.fir.serialization.FirElementSerializer
 import org.jetbrains.kotlin.fir.serialization.FirSerializerExtension
+import org.jetbrains.kotlin.fir.serialization.nonSourceAnnotations
 import org.jetbrains.kotlin.fir.types.*
 import org.jetbrains.kotlin.fir.types.impl.FirImplicitUnitTypeRef
 import org.jetbrains.kotlin.ir.declarations.IrClass
@@ -186,7 +187,7 @@ class FirJvmSerializerExtension @JvmOverloads constructor(
     }
 
     override fun serializeTypeParameter(typeParameter: FirTypeParameter, proto: ProtoBuf.TypeParameter.Builder) {
-        for (annotation in typeParameter.annotations) {
+        for (annotation in typeParameter.nonSourceAnnotations(session)) {
             proto.addExtension(JvmProtoBuf.typeParameterAnnotation, annotationSerializer.serializeAnnotation(annotation))
         }
     }

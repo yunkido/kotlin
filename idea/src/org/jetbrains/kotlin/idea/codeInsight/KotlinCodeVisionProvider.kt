4b194397d52bf9112b692fb960d1b8e07e55ecda
Andrei Klunnyi <andrei.klunni@jetbrains.com>
2020-04-21 18:09:06 +0200

    KT-38027 Support Code Vision feature in Kotlin
    preliminary: usages & inherits search

--- idea/src/org/jetbrains/kotlin/idea/codeInsight/KotlinCodeVisionProvider.kt
+++ idea/src/org/jetbrains/kotlin/idea/codeInsight/KotlinCodeVisionProvider.kt
@@ -6,12 +6,31 @@
 package org.jetbrains.kotlin.idea.codeInsight
 
 import com.intellij.codeInsight.hints.*
+import com.intellij.codeInsight.hints.presentation.AttributesTransformerPresentation
+import com.intellij.codeInsight.hints.presentation.InlayPresentation
+import com.intellij.codeInsight.hints.presentation.MouseButton
+import com.intellij.codeInsight.hints.presentation.PresentationFactory
+import com.intellij.codeInsight.hints.settings.InlayHintsConfigurable
+import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
 import com.intellij.lang.Language
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.markup.EffectType
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.search.searches.DirectClassInheritorsSearch
+import com.intellij.psi.search.searches.OverridingMethodsSearch
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.ui.layout.panel
+import org.jetbrains.kotlin.asJava.LightClassUtil
+import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.utils.SmartList
+import java.awt.Point
+import java.awt.event.MouseEvent
 import javax.swing.JPanel
 
 @Suppress("UnstableApiUsage")
@@ -53,12 +72,141 @@ class KotlinCodeVisionProvider : InlayHintsProvider<KotlinCodeVisionProvider.Kot
     }
 
     override fun getCollectorFor(
-        file: PsiFile,
-        editor: Editor,
-        settings: KotlinCodeVisionSettings,
+        file: PsiFile, editor: Editor, settings: KotlinCodeVisionSettings,
         sink: InlayHintsSink
     ): InlayHintsCollector? {
-        TODO("Not yet implemented")
+
+
+        return KotlinCodeVisionHintsCollector(editor, settings)
+    }
+
+
+    class KotlinCodeVisionHintsCollector(editor: Editor, val settings: KotlinCodeVisionSettings) : FactoryInlayHintsCollector(editor) {
+
+        private val FUS_GROUP_ID = "kotlin.code.vision"
+        private val USAGES_CLICKED_EVENT_ID = "usages.clicked"
+        private val IMPLEMENTATIONS_CLICKED_EVENT_ID = "implementations.clicked"
+        private val SETTING_CLICKED_EVENT_ID = "setting.clicked"
+
+
+        override fun collect(element: PsiElement, editor: Editor, sink: InlayHintsSink): Boolean {
+            if (element !is KtProperty && element !is KtNamedFunction && element !is KtClass && element !is KtConstructor<*>) {
+                return true
+            }
+
+            val hints: List<InlResult> = SmartList() // todo fill them
+
+            if (settings.showUsages) {
+                val toList = ReferencesSearch.search(element).toList()
+                //todo fill hints
+            }
+
+            if (settings.showImplementations) {
+                if (element is KtFunction) {
+                    //val lightClassMethod = LightClassUtil.getLightClassMethod(element)
+                    LightClassUtil.getLightClassMethod(element)?.let { it ->
+                        val toList = OverridingMethodsSearch.search(it, true).toList()
+                        //todo fill hints
+                    }
+                } else if (element is KtClass) {
+                    val lightClass = element.toLightClass()
+                    lightClass?.let {
+                        val list = DirectClassInheritorsSearch.search(it, element.useScope, true).toList()
+                        //todo fill hints
+                    }
+                }
+            }
+/*
+
+            if (usagesCount > 0) {
+
+                val offset = element.textRange.startOffset
+                val line: Int = editor.document.getLineNumber(offset)
+                val lineStart: Int = editor.document.getLineStartOffset(line)
+                val indent = offset - lineStart
+
+                val presentations = arrayOfNulls<InlayPresentation>(hints.size * 2 + 1)
+                presentations[0] = factory.text(StringUtil.repeat(" ", indent))
+                var o = 1
+                for (i in hints.indices) {
+                    val hint: InlResult = hints[i]
+                    if (i != 0) {
+                        presentations[o++] = factory.text(" ")
+                    }
+                    presentations[o++] = createPresentation(factory, element, editor, hint)
+                }
+                presentations[o] = factory.text("          ") // placeholder for "Settings..."
+
+
+                val seq = factory.seq(*presentations.requireNoNulls())
+                val withAppearingSettings = factory.changeOnHover(seq, {
+                    val trimmedSpace: Array<InlayPresentation> = Arrays.copyOf(presentations, presentations.size - 1)
+                    val spaceAndSettings =
+                        arrayOf(factory.text("  "), settings(factory, element, editor))
+                    val withSettings =
+                        ArrayUtil.mergeArrays(trimmedSpace, spaceAndSettings)
+                    factory.seq(*withSettings)
+                }) { e: MouseEvent? -> true }
+                sink.addBlockElement(lineStart, true, true, 0, withAppearingSettings)
+            }
+*/
+            return true
+        }
+
+        private fun createPresentation(
+            factory: PresentationFactory,
+            element: PsiElement,
+            editor: Editor,
+            result: InlResult
+        ): InlayPresentation {
+            val text = factory.smallText(result.getRegularText())
+            return factory.changeOnHover(text, {
+                val onClick = factory.onClick(
+                    text,
+                    MouseButton.Left
+                ) { event: MouseEvent?, _: Point? ->
+                    result.onClick(editor, element, event)
+
+                }
+                referenceColor(onClick)
+            }) { true }
+        }
+
+
+        private fun referenceColor(presentation: InlayPresentation): InlayPresentation {
+            return AttributesTransformerPresentation(
+                presentation
+            ) {
+                val attributes =
+                    EditorColorsManager.getInstance().globalScheme
+                        .getAttributes(EditorColors.REFERENCE_HYPERLINK_COLOR).clone()
+                attributes.effectType = EffectType.LINE_UNDERSCORE
+                attributes
+            }
+        }
+
+        private fun settings(factory: PresentationFactory, element: PsiElement, editor: Editor): InlayPresentation {
+            return createPresentation(factory, element, editor, object : InlResult {
+
+                override fun onClick(editor: Editor, element: PsiElement, event: MouseEvent?) {
+                    val project = element.project
+                    FUCounterUsageLogger.getInstance()
+                        .logEvent(project, FUS_GROUP_ID, SETTING_CLICKED_EVENT_ID)
+                    InlayHintsConfigurable.showSettingsDialogForLanguage(project, element.language)
+
+                }
+
+                override fun getRegularText(): String {
+                    return "Settings..."
+                }
+            })
+        }
+    }
+
+    interface InlResult {
+        fun onClick(editor: Editor, element: PsiElement, event: MouseEvent?)
+
+        fun getRegularText(): String
     }
 
     data class KotlinCodeVisionSettings(var showUsages: Boolean = false, var showImplementations: Boolean = false)

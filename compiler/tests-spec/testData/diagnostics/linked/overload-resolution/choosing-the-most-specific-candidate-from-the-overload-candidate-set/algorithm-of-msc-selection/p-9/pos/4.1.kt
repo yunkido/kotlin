f5d56f0990dc2b51ac5a1b5a5e3a611a080e166e
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2020-04-23 17:13:31 +0300

    [Spec tests] Add  few cases for generics

--- compiler/tests-spec/testData/diagnostics/linked/overload-resolution/choosing-the-most-specific-candidate-from-the-overload-candidate-set/algorithm-of-msc-selection/p-9/pos/4.1.kt
+++ compiler/tests-spec/testData/diagnostics/linked/overload-resolution/choosing-the-most-specific-candidate-from-the-overload-candidate-set/algorithm-of-msc-selection/p-9/pos/4.1.kt
@@ -22,9 +22,9 @@ fun case1(case: Case1) {
     //(1.1) return type is String
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(1)<!>
     //to (1.1)
-    case.<!DEBUG_INFO_CALL("fqName: boo; typeCall: extension function")!>boo(x=1)<!>
+    case.<!DEBUG_INFO_CALL("fqName: boo; typeCall: extension function")!>boo(x = 1)<!>
     //(1.1) return type is String
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(x=1)<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(x = 1)<!>
 
 }
 
@@ -34,7 +34,7 @@ fun Case1.boo(x: Short): Unit = TODO() //(1.3)
 fun Case1.boo(x: Byte): Unit = TODO() //(1.4)
 
 // TESTCASE NUMBER: 2
-class Case2{
+class Case2 {
     fun boo(x: Int, y: Int): String = TODO() //(1.1)
     fun boo(x: Long, y: Int): Unit = TODO() //(1.2)
     fun boo(x: Short, y: Int): Unit = TODO() //(1.3)
@@ -47,13 +47,13 @@ fun case2(case: Case2) {
     //(1.1) return type is String
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(1, 2)<!>
     //to (1.1)
-    case.<!DEBUG_INFO_CALL("fqName: Case2.boo; typeCall: function")!>boo(x=1, y=2)<!>
+    case.<!DEBUG_INFO_CALL("fqName: Case2.boo; typeCall: function")!>boo(x = 1, y = 2)<!>
     //(1.1) return type is String
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(x=1, y=2)<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(x = 1, y = 2)<!>
 }
 
 // TESTCASE NUMBER: 3
-class Case3{
+class Case3 {
     fun boo(x: Int, y: Int): String = TODO() //(1.1)
     fun boo(x: Int, y: Short): Unit = TODO() //(1.2)
 }
@@ -64,13 +64,13 @@ fun case3(case: Case3) {
     //(1.1) return type is String
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(1, 2)<!>
     //to (1.1)
-    case.<!DEBUG_INFO_CALL("fqName: Case3.boo; typeCall: function")!>boo(x=1, y=2)<!>
+    case.<!DEBUG_INFO_CALL("fqName: Case3.boo; typeCall: function")!>boo(x = 1, y = 2)<!>
     //(1.1) return type is String
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(x=1, y=2)<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.boo(x = 1, y = 2)<!>
 }
 
 // TESTCASE NUMBER: 4
-class Case4{
+class Case4 {
     operator fun plus(x: Int): String = TODO() //(1.1)
     operator fun plus(x: Long): Unit = TODO() //(1.2)
     operator fun plus(x: Short): Unit = TODO() //(1.3)
@@ -83,12 +83,48 @@ fun case4(case: Case4) {
     //(1.1) return type is String
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.plus(1)<!>
     //to (1.1)
-    case.<!DEBUG_INFO_CALL("fqName: Case4.plus; typeCall: operator function")!>plus(x=1)<!>
+    case.<!DEBUG_INFO_CALL("fqName: Case4.plus; typeCall: operator function")!>plus(x = 1)<!>
     //(1.1) return type is String
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.plus(x=1)<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case.plus(x = 1)<!>
     //as operator call case + 1
     //to (1.1)
-    <!DEBUG_INFO_CALL("fqName: Case4.plus; typeCall: operator function")!>case + 1<!>
+    <!DEBUG_INFO_CALL("fqName: Case4.plus; typeCall: operator function")!>case+1<!>
     //(1.1) return type is String
-    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case + 1<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>case+1<!>
+}
+
+// TESTCASE NUMBER: 5
+class Case5 {
+    fun List<Int>.foo(x: Int = 0): String = TODO()
+    fun List<Int>.foo(x: Short = 0): Unit = TODO()
+    fun List<Byte>.foo(x: Byte = 0): Unit = TODO()
+
+    fun case(list: List<Int>) {
+        list.<!DEBUG_INFO_CALL("")!>foo(1)<!>
+        <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>list.foo(1)<!>
+    }
+}
+
+// TESTCASE NUMBER: 6
+class Case6 {
+    fun List<Int>.foo(x: Int, y: Int = 1): String = TODO()
+
+    fun <T> List<Int>.foo(x: T): Unit = TODO()
+
+    fun case(list: List<Int>) {
+        list.<!DEBUG_INFO_CALL("")!>foo(1)<!>
+        <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>list.foo(1)<!>
+    }
+}
+
+// TESTCASE NUMBER: 7
+class Case7 {
+    fun <T> List<T>.foo(x: T, y: Int): String = TODO()
+
+    fun <T> List<T>.foo(x: T, y: Short): Unit = TODO()
+
+    fun case(list: List<Short>) {
+        list.<!DEBUG_INFO_CALL("")!>foo(1, 1)<!>
+        <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>list.foo(1, 1)<!>
+    }
 }
